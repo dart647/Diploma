dfab4ba47f35b14fb031149a8fb9e0f24da9aca8
dart647 <spivak1999@yandex.ru>
2020-01-14 23:17:36 +0300

    Добавьте файлы проекта.

---
+++ JournalForSecurity/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JournalForSecurity.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+
+    }
+}
